10b12da7123dcd08f6bf69b86043f1bc9620e3f5
Bianca Pagani <paganibianca08@gmail.com>
2024-11-16 10:49:09 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,73 @@
-criaCartao(
+perguntas.js
+criaCartao (
     'Programação',
     'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'O Python é uma linguagem de Programação.'
 )
 
-criaCartao(
+criaCartao (
     'Geografia',
     'Qual a capital da França?',
-    'A capital da França é Paris'
+    'A capital da França é Paris.'
 )
 
-criaCartao(
+criaCartao (
     'Programação',
     'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Uma função é um bloco de códigos que executa alguma tarefa.'
 )
 
-criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+criaCartao (
+    'Lingua Inglesa',
+    'Como se diz vermelho em Inglês?',
+    'Vermelho em Inglês é RED.'
+)
+
+criaCartao (
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasília.'
+)
+
+criaCartao (
+    'Astrologia',
+    'Qual o maior planeta do sistema solar?',
+    'O maior planeta é Jupter.'
+)
+
+criaCartao (
+    'Biologia',
+    'O que é o processo de fotossíntese?',
+    'Fotossíntese é o processo pelo qual as plantas produzem energia a partir da luz solar, água e dióxido de carbono, liberando oxigênio.'
+)
+
+criaCartao (
+    'Física',
+    'Quem foi Albert Einstein?',
+    'Albert Einstein foi um físico teórico alemão conhecido por desenvolver a teoria da relatividade.'
+)
+
+criaCartao (
+    'Portugues',
+    'O que é um substantivo?',
+    'Substantivo é a classe de palavras que nomeia seres, objetos, lugares, sentimentos, entre outros.'
+)
+
+criaCartao (
+    'História',
+    'Em que ano o Brasil foi descoberto por Portugal?',
+    'O Brasil foi descoberto em 1500'
+)
+
+criaCartao (
+    'Filosofia',
+    'Quem foi Sócrates?',
+    'Sócrates foi um filósofo grego considerado um dos fundadores da filosofia ocidental.'
+)
+
+
+criaCartao (
+    'Química',
+    'Qual o elemento 7 da tabela periódica?',
+    'Nitrogênio.'
 )
